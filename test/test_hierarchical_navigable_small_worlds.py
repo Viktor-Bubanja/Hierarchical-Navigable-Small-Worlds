bb83b55c9dbcb0673f363fa14462eeed6475c771
Viktor Bubanja <viktor.bubanja@heyjobs.de>
2024-04-24 07:54:17 +0200

    Add test for add method

--- test/test_hierarchical_navigable_small_worlds.py
+++ test/test_hierarchical_navigable_small_worlds.py
@@ -15,12 +15,12 @@ def hnsw():
 
 
 @pytest.fixture
-def setup_hnsw():
+def populated_hnsw():
     hnsw = HNSW(M=2)
     vertices = [Vertex(vector=[i], num_layers=hnsw.num_layers) for i in range(5)]
     for vertex in vertices:
         hnsw.add(vertex)
-    return hnsw, vertices
+    return hnsw
 
 
 def test_knn_returns_k_closest_vectors_to_given_vector():
@@ -63,56 +63,45 @@ def test_add_sets_entry_point_if_index_is_empty():
     assert insertion_layer == 5, "The first vertex should be inserted in the last layer."
 
 
-def test_graph_integrity_after_addition(mocker, setup_hnsw):
-    mock_values = [0, 0, 1, 0, 0, 0, 1, 1]
-    hnsw, _ = setup_hnsw()
-    mocker.patch.object(hnsw, '_get_random_level', side_effect=mock_values)
-    new_vertex = Vertex(vector=[10], num_layers=hnsw.num_layers)
-    insertion_layer = hnsw.add(new_vertex)
+def test_add_sets_edges_between_vertices_in_both_directions(populated_hnsw):
+    new_vertex = Vertex(vector=[10], num_layers=populated_hnsw.num_layers)
+    insertion_layer = populated_hnsw.add(new_vertex)
     for i in range(insertion_layer, -1, -1):
         edges = new_vertex.get_edges_in_layer(i)
+        assert edges, "There should be edges in the layer."
         for edge in edges:
             assert new_vertex in edge.get_edges_in_layer(i), "Edges should be bidirectional."
 
 
-def test_add_sets_edges_of_input_vertex_as_well_as_edges_of_neighbours_in_all_necessary_layers(mocker, hnsw):
-    mock_values = [0, 0, 1, 0, 0, 0, 1, 1]  # The layers that the input vertices will be inserted in
-    mocker.patch.object(hnsw, '_get_random_level', side_effect=mock_values)
+def test_add_sets_edges_of_input_vertex_as_well_as_edges_of_neighbours_in_all_necessary_layers(mocker):
+    mock_random_levels = [0, 0, 1, 0, 0, 0, 1, 1]  # The layers that the input vertices will be inserted in
+    mocker.patch.object(HNSW, '_get_random_level', side_effect=mock_random_levels)
+    mock_probabilities = [0.5, 0.5]  # The probabilities of inserting an edge into each layer
+    mocker.patch.object(HNSW, '_set_layer_probabilities', return_value=mock_probabilities)
+    hnsw = HNSW(M=2)
     input_vertices = [
         Vertex(
-            vector=[1,1,1],
+            vector=[0, 0, 0],
             num_layers=hnsw.num_layers
         ),
         Vertex(
-            vector=[2,2,2],
+            vector=[1, 1, 1],
             num_layers=hnsw.num_layers
         ),
         Vertex(
-            vector=[3,3,3],
+            vector=[2, 2, 2],
             num_layers=hnsw.num_layers
         ),
         Vertex(
-            vector=[4,4,4],
+            vector=[3, 3, 3],
             num_layers=hnsw.num_layers
         ),
         Vertex(
-            vector=[5,5,5],
+            vector=[4, 4, 4],
             num_layers=hnsw.num_layers
         ),
         Vertex(
-            vector=[6,6,6],
-            num_layers=hnsw.num_layers
-        ),
-        Vertex(
-            vector=[7,7,7],
-            num_layers=hnsw.num_layers
-        ),
-        Vertex(
-            vector=[8,8,8],
-            num_layers=hnsw.num_layers
-        ),
-        Vertex(
-            vector=[9,9,9],
+            vector=[5, 5, 5],
             num_layers=hnsw.num_layers
         ),
     ]
@@ -120,4 +109,20 @@ def test_add_sets_edges_of_input_vertex_as_well_as_edges_of_neighbours_in_all_ne
         hnsw.add(x=input_vertex)
 
     assert hnsw.entry_point == input_vertices[0]
-    assert input_vertices[0].edges == [input_vertices[1], input_vertices[2], input_vertices[3], input_vertices[4], input_vertices[5], input_vertices[6], input_vertices[7]]
+    assert input_vertices[0].edges[0] == {Vertex(vector=[1, 1, 1]), Vertex(vector=[2, 2, 2]), Vertex(vector=[3, 3, 3]), Vertex(vector=[4, 4, 4])}
+    assert input_vertices[0].edges[1] == {Vertex(vector=[3, 3, 3])}
+
+    assert input_vertices[1].edges[0] == {Vertex(vector=[0, 0, 0]), Vertex(vector=[2, 2, 2]), Vertex(vector=[3, 3, 3]), Vertex(vector=[4, 4, 4]), Vertex(vector=[5, 5, 5])}
+    assert input_vertices[1].edges[1] == set()
+
+    assert input_vertices[2].edges[0] == {Vertex(vector=[1, 1, 1]), Vertex(vector=[0, 0, 0]), Vertex(vector=[3, 3, 3]), Vertex(vector=[4, 4, 4]), Vertex(vector=[5, 5, 5])}
+    assert input_vertices[2].edges[1] == set()
+
+    assert input_vertices[3].edges[0] == {Vertex(vector=[2, 2, 2]), Vertex(vector=[1, 1, 1]), Vertex(vector=[0, 0, 0]), Vertex(vector=[4, 4, 4]), Vertex(vector=[5, 5, 5])}
+    assert input_vertices[3].edges[1] == {Vertex(vector=[0, 0, 0])}
+
+    assert input_vertices[4].edges[0] == {Vertex(vector=[3, 3, 3]), Vertex(vector=[2, 2, 2]), Vertex(vector=[1, 1, 1]), Vertex(vector=[0, 0, 0]), Vertex(vector=[5, 5, 5])}
+    assert input_vertices[4].edges[1] == set()
+
+    assert input_vertices[5].edges[0] == {Vertex(vector=[4, 4, 4]), Vertex(vector=[3, 3, 3]), Vertex(vector=[2, 2, 2]), Vertex(vector=[1, 1, 1])}
+    assert input_vertices[5].edges[1] == set()
