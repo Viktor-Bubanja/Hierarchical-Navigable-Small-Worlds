5621b23b2fe2d3021c6ccedc4e67907c0309cc13
Viktor Bubanja <viktor.bubanja@heyjobs.de>
2024-05-17 10:49:03 +0100

    Tidy up tests with populated HNSW fixture

--- test/test_hierarchical_navigable_small_worlds.py
+++ test/test_hierarchical_navigable_small_worlds.py
@@ -23,6 +23,45 @@ def populated_hnsw():
     return hnsw
 
 
+@pytest.fixture
+def popoulated_hnsw_2(mocker):
+    mock_random_levels = [0, 0, 1, 0, 0, 0, 1, 1]  # The layers that the input vertices will be inserted in
+    mocker.patch.object(HNSW, '_get_random_level', side_effect=mock_random_levels)
+    mock_probabilities = [0.5, 0.5]  # The probabilities of inserting an edge into each layer
+    mocker.patch.object(HNSW, '_set_layer_probabilities', return_value=mock_probabilities)
+    hnsw = HNSW(M=2)
+    input_vertices = [
+        Vertex(
+            vector=[0, 0, 0],
+            num_layers=hnsw.num_layers
+        ),
+        Vertex(
+            vector=[1, 1, 1],
+            num_layers=hnsw.num_layers
+        ),
+        Vertex(
+            vector=[2, 2, 2],
+            num_layers=hnsw.num_layers
+        ),
+        Vertex(
+            vector=[3, 3, 3],
+            num_layers=hnsw.num_layers
+        ),
+        Vertex(
+            vector=[4, 4, 4],
+            num_layers=hnsw.num_layers
+        ),
+        Vertex(
+            vector=[5, 5, 5],
+            num_layers=hnsw.num_layers
+        ),
+    ]
+    for input_vertex in input_vertices:
+        hnsw.add(x=input_vertex)
+    
+    return hnsw, input_vertices
+
+
 def test_knn_returns_k_closest_vectors_to_given_vector():
     neighbours = [
         Vertex(vector=[1.5, 0.5, 1.5]),
@@ -73,41 +112,8 @@ def test_add_sets_edges_between_vertices_in_both_directions(populated_hnsw):
             assert new_vertex in edge.get_edges_in_layer(i), "Edges should be bidirectional."
 
 
-def test_add_sets_edges_of_input_vertex_as_well_as_edges_of_neighbours_in_all_necessary_layers(mocker):
-    mock_random_levels = [0, 0, 1, 0, 0, 0, 1, 1]  # The layers that the input vertices will be inserted in
-    mocker.patch.object(HNSW, '_get_random_level', side_effect=mock_random_levels)
-    mock_probabilities = [0.5, 0.5]  # The probabilities of inserting an edge into each layer
-    mocker.patch.object(HNSW, '_set_layer_probabilities', return_value=mock_probabilities)
-    hnsw = HNSW(M=2)
-    input_vertices = [
-        Vertex(
-            vector=[0, 0, 0],
-            num_layers=hnsw.num_layers
-        ),
-        Vertex(
-            vector=[1, 1, 1],
-            num_layers=hnsw.num_layers
-        ),
-        Vertex(
-            vector=[2, 2, 2],
-            num_layers=hnsw.num_layers
-        ),
-        Vertex(
-            vector=[3, 3, 3],
-            num_layers=hnsw.num_layers
-        ),
-        Vertex(
-            vector=[4, 4, 4],
-            num_layers=hnsw.num_layers
-        ),
-        Vertex(
-            vector=[5, 5, 5],
-            num_layers=hnsw.num_layers
-        ),
-    ]
-    for input_vertex in input_vertices:
-        hnsw.add(x=input_vertex)
-
+def test_add_sets_edges_of_input_vertex_as_well_as_edges_of_neighbours_in_all_necessary_layers(popoulated_hnsw_2):
+    hnsw, input_vertices = popoulated_hnsw_2
     assert hnsw.entry_point == input_vertices[0]
     assert input_vertices[0].edges[0] == {Vertex(vector=[1, 1, 1]), Vertex(vector=[2, 2, 2]), Vertex(vector=[3, 3, 3]), Vertex(vector=[4, 4, 4])}
     assert input_vertices[0].edges[1] == {Vertex(vector=[3, 3, 3])}
